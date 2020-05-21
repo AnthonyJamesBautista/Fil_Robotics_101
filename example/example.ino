98b58a53e87ad7bda9b5f503047851062ade349b
ANTON <anton_bautista_tm@yahoo.com>
2020-05-21 15:24:34 +0800

    first commit

--- example/example.ino
+++ example/example.ino
@@ -1,5 +1,5 @@
-#include <deg2utm.h>
 #include <GPS_module.h>
+#include <deg2utm.h>
 
 //variable declarations for latitude and longitude
 double lt;
@@ -9,20 +9,20 @@ double UTMNorthing;
 double UTMEasting;
 
 void setup() {
-Serial.begin (9600);
-GPS_initialise(); //initilize GPS module library
+  Serial.begin (9600);
+  GPS_initialise(); //initilize GPS module library
 }
 
 void loop() {
-if (gps_avail())
-{
-lt = get_lat();
-ln = get_lon();
-deg2utm(lt, ln, &UTMNorthing, &UTMEasting);
-//Print UTM coordinates in Serial monitor
-Serial.print("Northing: "); Serial.print(UTMNorthing, 3);
-Serial.print ("\t");
-Serial.print ("Easting: "); Serial.println(UTMEasting, 3);
-delay (100);
-}
+  if (gps_avail())
+  {
+    lt = get_lat();
+    ln = get_lon();
+    deg2utm(lt, ln, &UTMNorthing, &UTMEasting);
+    //Print UTM coordinates in Serial monitor
+    Serial.print("Latitude: "); Serial.print(lt, 3); Serial.print ("\t"); Serial.print("Longitude: "); Serial.println(ln, 3);
+    Serial.print("Northing: "); Serial.print(UTMNorthing, 3); Serial.print ("\t"); Serial.print ("Easting: "); Serial.println(UTMEasting, 3);
+    Serial.println("");
+    delay (100);
+  }
 }
